eacf06af10dead62558f3e9fc0a2c107824e60a7
h12345jack <h12345jack@163.com>
2016-11-30 19:45:08 +0800

    front

---
+++ Search_system/const.py
@@ -0,0 +1,10 @@
+MYSQL_CONFIG = {
+    'HOST'     : '127.0.0.1',
+    'USERNAME' : 'root',
+    'PASSWORD' : 'admin',
+    'DBNAME'   : 'XFS_DB',
+}
+
+XML_DIR = '../Spider/xml_dir'
+
+METADATA_DIR = '../Spider/metadata_dir'
